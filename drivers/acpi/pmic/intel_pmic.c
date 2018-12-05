deb701546ad75c0f33a02e8b35b376deb91a0a0b
Hans de Goede <hdegoede@redhat.com>
2018-12-05 19:27:06 +0100

    ACPI / PMIC: Add support for executing PMIC MIPI sequence elements
    DSI LCD panels describe an initialization sequence in the Video BIOS
    Tables using so called MIPI sequences. One possible element in these
    sequences is a PMIC specific element of 15 bytes.
    
    Although this is not really an ACPI opregion, the ACPI opregion code is the
    closest thing we have. We need to have support for these PMIC specific MIPI
    sequence elements somwhere. Since we already instantiate a special platform
    device for Intel PMICs for the ACPI PMIC OpRegion handler to bind to,
    with PMIC specific implementations of the OpRegion, the handling of MIPI
    sequence PMIC elements fits very well in the ACPI PMIC OpRegion code.
    
    This commit adds a new intel_soc_pmic_exec_mipi_pmic_seq_element()
    function, which is to be backed by a PMIC specific
    exec_mipi_pmic_seq_element callback. This function will be called by the
    i915 code to execture MIPI sequence PMIC elements.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>

--- drivers/acpi/pmic/intel_pmic.c
+++ drivers/acpi/pmic/intel_pmic.c
@@ -15,6 +15,7 @@
 
 #include <linux/export.h>
 #include <linux/acpi.h>
+#include <linux/mfd/intel_soc_pmic.h>
 #include <linux/regmap.h>
 #include <acpi/acpi_lpat.h>
 #include "intel_pmic.h"
@@ -35,6 +36,8 @@ struct intel_pmic_opregion {
 	struct intel_pmic_opregion_data *data;
 	struct intel_pmic_regs_handler_ctx ctx;
 };
+
+static struct intel_pmic_opregion *intel_pmic_opregion;
 
 static int pmic_get_reg_bit(int address, struct pmic_table *table,
 			    int count, int *reg, int *bit)
@@ -304,6 +307,7 @@ int intel_pmic_install_opregion_handler(struct device *dev, acpi_handle handle,
 	}
 
 	opregion->data = d;
+	intel_pmic_opregion = opregion;
 	return 0;
 
 out_remove_thermal_handler:
@@ -319,3 +323,24 @@ out_error:
 	return ret;
 }
 EXPORT_SYMBOL_GPL(intel_pmic_install_opregion_handler);
+
+void intel_soc_pmic_exec_mipi_pmic_seq_element(const u8 *data)
+{
+	struct intel_pmic_opregion_data *d;
+
+	if (!intel_pmic_opregion) {
+		pr_warn("%s: No PMIC registered\n", __func__);
+		return;
+	}
+
+	d = intel_pmic_opregion->data;
+	if (!d->exec_mipi_pmic_seq_element) {
+		pr_warn("%s: Not implemented\n", __func__);
+		return;
+	}
+
+	mutex_lock(&intel_pmic_opregion->lock);
+	d->exec_mipi_pmic_seq_element(intel_pmic_opregion->regmap, data);
+	mutex_unlock(&intel_pmic_opregion->lock);
+}
+EXPORT_SYMBOL_GPL(intel_soc_pmic_exec_mipi_pmic_seq_element);
